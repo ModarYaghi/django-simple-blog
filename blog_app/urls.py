560248503ce0c3d14534c616e3debc7b3d5be79a
ModarYaghi <mfyaghi81@gmail.com>
2025-03-12 17:47:56 +0300

    switch to GCBVs

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,9 +1,15 @@
 # blog/urls.py
 from django.urls import path
 
-from .views import post_detail_view, post_list_view
+# from .views import post_detail_view, post_list_view
+from .views import BlogDetailView, BlogListView  # import the generic clas-based views
 
 urlpatterns = [
-    path("post/<int:pk>/", post_detail_view, name="post_detail"),
-    path("", post_list_view, name="home"),
+    # path("post/<int:pk>/", post_detail_view, name="post_detail"),
+    #
+    # this is how to build url for generic class-based view
+    path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
+    #
+    # path("", post_list_view, name="home"),
+    path("", BlogListView.as_view(), name="home"),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import get_object_or_404, render
+# from django.shortcuts import get_object_or_404, render
+from django.views.generic import (  # to switch to generic class-based views
+    DetailView,
+    ListView,
+)
 
 from .models import Post
 
@@ -6,12 +10,29 @@ from .models import Post
 #
 
 
-def post_list_view(request):
-    posts = Post.objects.all()  # type: ignore
-    return render(request, "home.html", {"posts": posts})
+# def post_list_view(request):
+#     posts = Post.objects.all()  # type: ignore
+#     return render(request, "home.html", {"posts": posts})
+#
 
 
-def post_detail_view(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+class BlogListView(ListView):
+    model = Post  # the Post model we want to view as list
+    template_name = "home.html"  # the template will present this view.
+    #
+    # unnecessary, but it makes thigs esier to understand.
+    context_object_name = "posts"
 
-    return render(request, "post_detail.html", {"post": post})
+
+# def post_detail_view(request, pk):
+#     post = get_object_or_404(Post, pk=pk)
+#
+#     return render(request, "post_detail.html", {"post": post})
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
+    #
+    # unnecessary, but it makes things esier to understand.
+    contex_object_name = "post"
